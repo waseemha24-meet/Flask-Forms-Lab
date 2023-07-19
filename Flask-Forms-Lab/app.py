fb5ddc9f2fcb03478f1bfec305d410dab25f964d
waseemha24-meet <waseemhak10@gmail.com>
2023-07-19 11:53:57 +0300

    facebook

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -8,19 +8,32 @@ app = Flask(  # Create a flask app
 )
 
 
-username = "llo2ay"
-password = "123"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+usernamex = "waseem"
+passwordx = "123"
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/' , methods=['GET', 'POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
+	if request.method == 'POST':
+		username=request.form['username']
+		password=request.form['password']
+		if usernamex == username and passwordx == password:
+			return redirect(url_for('home'))
+	else:
+		return render_template('login.html')
 
 
+facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+@app.route('/home' , methods=['GET', 'POST'] , )
+def home():
+	return render_template('home.html' , friends = facebook_friends)
+
+
+@app.route('/friend_axists/<string:name>' , methods=['GET', 'POST'])
+def hello_name_route(name):
+	return render_template('friend_exists.html' , methods=['GET', 'POST'] , facebook_friends=facebook_friends , name = name)
 
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
-    debug=True
+		debug=True
 	)
